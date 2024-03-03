ebeb47b0300d8c1e6fc31af43291e1fcf92edd71
mKozhemiakin <m.kozhemiakin@cez.gov.pl>
2024-03-03 14:21:25 +0100

    prettier

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
